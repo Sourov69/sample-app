49d720aea099dfdc6ce2252cfaa2c7897d34a9e2
Sourovtalukder69 <sourovtalukder69@gmail.com>
2024-09-06 16:13:26 +0600

    initial comment adding two file

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title('Sourov Talukder')
+col1, col2 = st.columns(2)
+with col1:
+    st.image('sourov.jpg')
+with col2:
+    st.write("""
+Hi i am Sourov 
+\nI am a Data Scientist 
+\nBelow i Created a lot of project""")
