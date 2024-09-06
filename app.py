2e66dd5db3f7bd2a855e158130a79143dbff1c58
Sourovtalukder69 <sourovtalukder69@gmail.com>
2024-09-06 19:45:00 +0600

    add courses to homepage

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ col1, col2 = st.columns(2)
 with col1:
     st.image('sourov.jpg')
 with col2:
-    st.write("""
-Hi i am Sourov 
-\nI am a Data Scientist 
-\nBelow i Created a lot of project""")
+    st.write("""Hi i am Sourov 
+             \nI am a Data Scientist 
+             \nBelow i Created a lot of project""")
+    
+st.header('Projects')
+st.subheader('Data Science')
+st.subheader('Data Analysis')
+st.subheader('Data Engineering')    
